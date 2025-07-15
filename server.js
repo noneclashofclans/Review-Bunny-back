976a5fd7ae60aa8e923d68b6b0486b3af2a5e19f
noneclashofclans <rishitmohanty3@gmail.com>
2025-07-15 20:06:01 +0530

    added port opt

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const PORT = process.env.PORT || 3000;
 require('dotenv').config()
 
 const app = require('./src/app')
 
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if (err) console.log(err.message)
     else console.log('Server is running')
 })
